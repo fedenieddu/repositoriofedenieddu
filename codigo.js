ef91d8ba908f194d805e7eb21911baac51e04b29
Federico Nieddu <federiconieddu@Federicos-MacBook-Pro.local>
2022-06-21 14:24:12 -0300

    actualizacion

--- codigo.js
+++ codigo.js
@@ -1,6 +1,9 @@
 const descripcionmercedes = document.getElementById("descmerc");
 const descripcionferrari = document.getElementById("descferrari");
 const descripcionredbull = document.getElementById("descrb");
+const descripcionfotom = document.getElementById("descripcion_foto_mercedes");
+const descripcionfotof = document.getElementById("descripcion_foto_ferrari");
+const descripcionfotor= document.getElementById("descripcion_foto_rb");
 
 const cargarImagen = (entradas, observador) => {
   entradas.forEach((entrada) => {
@@ -12,8 +15,11 @@ const cargarImagen = (entradas, observador) => {
 const observador = new IntersectionObserver(cargarImagen, {
   root: null,
   rootMargin: "0px 0px 0px 0px",
-  threshold: 0.7,
+  threshold: 0.2,
 });
 observador.observe(descripcionmercedes);
 observador.observe(descripcionferrari);
 observador.observe(descripcionredbull);
+observador.observe(descripcionfotom);
+observador.observe(descripcionfotof);
+observador.observe(descripcionfotor);
